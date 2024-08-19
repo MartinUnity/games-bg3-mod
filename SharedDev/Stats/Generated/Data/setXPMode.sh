10259356f69354344297fbb809b53ff4a85a8e4f
Martin Nielsen <martin@skytech.dk>
2024-08-19 11:36:32 +0200

    Fixed hardlinking files

--- SharedDev/Stats/Generated/Data/setXPMode.sh
+++ SharedDev/Stats/Generated/Data/setXPMode.sh
@@ -5,14 +5,14 @@ MODE=$1
 rm XPData.txt
 if [ "${MODE}" == "FAST" ]; then
     echo "Setting XP to FAST"
-    ln -s XPData-FAST.txt XPData.txt
+    ln XPData-FAST.txt XPData.txt
 elif [ "${MODE}" == "NORMAL" ]; then
     echo "Resetting to default"
     rm XPData.txt
 elif [ "${MODE}" == "80" ]; then
     echo "Setting XP to -80"
-    ln -s XPData-80.txt XPData.txt
+    ln XPData-80.txt XPData.txt
 else
     echo "Default :: Setting XP to normal"
-    ln -s XPData-67.txt XPData.txt
+    ln XPData-67.txt XPData.txt
 fi
